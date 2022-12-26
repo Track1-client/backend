c3616ffe592d74f5cb2efc87e4616639f5881598
dahyunkim <dahyun_ee@naver.com>
2022-12-27 00:54:25 +0900

    [feature] CD update

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -2,7 +2,6 @@
 REPOSITORY=/home/ubuntu/build
 sudo pm2 kill
 cd $REPOSITORY
-cd server
-sudo rm -rf node_modules
+
 sudo yarn install --frozen-lockfile
 sudo pm2 start dist
--- scripts/before-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-REPOSITORY=/home/ubuntu/build
-sudo pm2 kill
-cd $REPOSITORY
-
-sudo rm -rf server
