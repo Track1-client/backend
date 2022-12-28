291680afcc393cc8a70e0cdfefd6f4289919b1be
dahyunkim <dahyun_ee@naver.com>
2022-12-29 03:33:32 +0900

    [feature] cors 추가

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000; 
+const corsOptions = {
+  origin: '*',
+  credential: true,
+}
 
 app.use(express.json());
-
+app.use(cors(corsOptions));
 app.use("/", router); 
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
