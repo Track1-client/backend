eb24a6a3fd7ca6c9831d466fafd0389c76f20d7e
dahyunkim <dahyun_ee@naver.com>
2023-01-03 02:07:14 +0900

    [feature] vocal portfolio api 완성

--- src/controller/mypageController.ts
+++ src/controller/mypageController.ts
@@ -3,7 +3,7 @@ import { rm, sc } from '../constants';
 import { fail, success } from '../constants/response';
 import { getAudioDurationInSeconds } from 'get-audio-duration';
 import config from '../config';
-import { ProducerPortfolioDTO } from '../interfaces/mypage';
+import { ProducerPortfolioDTO, VocalPortfolioDTO } from '../interfaces/mypage';
 import convertCategory from '../modules/convertCategory';
 import { mypageService } from '../service';
 
@@ -27,8 +27,29 @@ const createProducerPortfolio = async(req: Request, res: Response) => {
     return res.status(sc.CREATED).send(success(sc.CREATED, rm.PRODUCER_PORTFOLIO_UPLOAD_SUCCESS, {"producerPortfolioId": data.id}));
 };
 
+const createVocalPortfolio = async(req: Request, res: Response) => {
+    const myfiles = JSON.parse(JSON.stringify(req.files));
+
+    //* 재킷이미지 없는 경우 -> default jacketImage로 설정
+    const jacketImageLocation = !("jacketImage" in myfiles) ? config.defaultBeatJacketImage : myfiles['jacketImage'][0]['location'] as string;
+
+    //* wavFile 없는 경우 -> 오류 반환
+    if(!("wavFile" in myfiles)) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NO_WAV_FILE));
+    const wavFilelocation = myfiles['wavFile'][0]['location'] as string;
+
+    const portfolioDTO: VocalPortfolioDTO = req.body; 
+
+    portfolioDTO.category = convertCategory(portfolioDTO.category);
+
+    const data = await mypageService.postVocalPortfolio(portfolioDTO, jacketImageLocation as string, wavFilelocation as string);
+    if (!data) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.VOCAL_PORTFOLIO_UPLOAD_FAIL));
+
+    return res.status(sc.CREATED).send(success(sc.CREATED, rm.VOCAL_PORTFOLIO_UPLOAD_SUCCESS, {"vocalPortfolioId": data.id}));
+};
+
 const mypageController = {
     createProducerPortfolio,
+    createVocalPortfolio,
 };
 
 export default mypageController;
--- src/service/mypageService.ts
+++ src/service/mypageService.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-import { ProducerPortfolioDTO } from '../interfaces/mypage';
+import { ProducerPortfolioDTO, VocalPortfolioDTO } from '../interfaces/mypage';
 
 const prisma = new PrismaClient();
 
-const postProducerPortfolio  = async(portfolioDTO: ProducerPortfolioDTO, jacketLocation: string, wavLocation: string) => {
+const postProducerPortfolio = async(portfolioDTO: ProducerPortfolioDTO, jacketLocation: string, wavLocation: string) => {
     const data = await prisma.producerPortfolio.create({
         data: {
             title: portfolioDTO.title,
@@ -19,8 +19,25 @@ const postProducerPortfolio  = async(portfolioDTO: ProducerPortfolioDTO, jacketL
     return data;
 };
 
+const postVocalPortfolio = async(portfolioDTO: VocalPortfolioDTO, jacketLocation: string, wavLocation: string) => {
+    const data = await prisma.vocalPortfolio.create({
+        data: {
+            title: portfolioDTO.title,
+            category: portfolioDTO.category,
+            vpfFile: wavLocation,
+            vpfImage: jacketLocation,
+            content: portfolioDTO.introduce,
+            keyword: portfolioDTO.keyword,
+            vocalId: portfolioDTO.userId,
+        },
+    });
+
+    return data;
+};
+
 const mypageService = {
     postProducerPortfolio,
+    postVocalPortfolio,
 };
 
 export default mypageService;
