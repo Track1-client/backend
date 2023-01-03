8595cbd7e4b76232d0d5dfcd3c3fa19d085b5441
Yewon So <soyw96@gmail.com>
2023-01-04 02:19:42 +0900

    [docs] 충돌 수정

--- src/interfaces/tracks/index.ts
+++ src/interfaces/tracks/index.ts
@@ -1,7 +1,8 @@
 export { default as BeatCreateDTO } from './BeatCreateDTO';
-<<<<<<< HEAD
-export { default as BeatClickedDTO } from './BeatClickedDTO';
-=======
 export { default as BeatDownloadReturnDTO } from './BeatFileDownloadReturnDTO';
 export { default as AllBeatDTO } from './AllBeatDTO';
->>>>>>> 187e448c6a50d7e2c71fddac22db37fd71273edc
+export { default as BeatClickedDTO } from './BeatClickedDTO';
+
+
+
+
