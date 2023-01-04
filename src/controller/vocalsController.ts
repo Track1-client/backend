c621dc991fafcfecff0e9b7bfe253d1b4cdc2cf2
dahyunkim <dahyun_ee@naver.com>
2023-01-05 03:15:15 +0900

    [feature] 필터링해서 보컬 정렬 완성

--- src/controller/vocalsController.ts
+++ src/controller/vocalsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { rm, sc } from '../constants';
 import { fail, success } from '../constants/response';
+import convertCategory from '../modules/convertCategory';
 import { vocalsService } from '../service';
 
 
@@ -11,8 +12,19 @@ const getAllVocals = async(req: Request, res: Response) => {
     return res.status(sc.OK).send(success(sc.OK, rm.GET_VOCAL_LIST_SUCCESS, {"vocalList": data}));
 };
 
+const getFilteringVocals = async(req: Request, res: Response) => {
+    const { categ } = req.query;
+
+    const data = await vocalsService.getFilteredVocals(await convertCategory(categ as string));
+    if(!data) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.GET_VOCAL_LIST_FAIL)); 
+
+    return res.status(sc.OK).send(success(sc.OK, rm.GET_VOCAL_LIST_SUCCESS, {"vocalList": data}));
+
+};
+
 const vocalsController = {
     getAllVocals,
+    getFilteringVocals,
 };
 
 export default vocalsController;
