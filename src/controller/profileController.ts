fd8afe324db08000a1eb967f25e63ac9fa3ebf14
dahyunkim <dahyun_ee@naver.com>
2023-01-05 20:00:03 +0900

    [docs] main pull용 commit

--- src/controller/profileController.ts
+++ src/controller/profileController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import { rm, sc } from '../constants';
 import { fail, success } from '../constants/response';
-import { getAudioDurationInSeconds } from 'get-audio-duration';
 import { profileService } from "../service";
 
-const getProducerProfile= async(req:Request, res:Response) => {
+const getProducerProfile = async(req: Request, res: Response) => {
 
     const { producerId } = req.params;
     const { userId, tableName } = req.body;
@@ -13,10 +12,17 @@ const getProducerProfile= async(req:Request, res:Response) => {
     if(!data) return res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.INVALID_BEAT_ID));
 
     return res.status(sc.OK).send(success(sc.OK, rm.READ_ALL_COMMENT_SUCCESS, {"commentList": data}));
-}
+};
+
+const getOpenedBeats = async(req: Request, res: Response) => {
+    
+    const { beatId } = req.params;
+
+};
 
 const profileController = {
     getProducerProfile,
+    getOpenedBeats,
 };
 
 export default profileController;
